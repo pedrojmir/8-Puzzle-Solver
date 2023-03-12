4f01d44f60b0c48732ef8763a4d550ada730eafc
charredtomatoes7 <mirreynoso@usf.edu>
2023-03-12 15:37:43 -0400

    Pulled changes from Cory's-Branch

--- dfs.py
+++ dfs.py
@@ -1,7 +1,7 @@
 import time
 
-initial_puzzle = [6,1,2,3,4,5,8,7,0]
-goal_state =     [1,2,3,4,5,6,7,8,0]
+initial_puzzle = [1,2,3,4,5,6,7,8,0]
+goal_state = [1,8,2,0,4,3,7,6,5]
 
 # Check if initial puzzle is solvable by counting the number of inversions in the initial state and the goal state
 # If the number of inversions is odd, the puzzle is not solvable
@@ -20,14 +20,6 @@ def is_solvable(initial_state, goal_state):
     # If the number of inversions is odd, the puzzle is not solvable
     return inversions % 2 == 0
 
-# initial_puzzle = [1,2,3,
-#                   4,5,0,
-#                   7,8,6]
-# goal_state =     [1,2,3,
-#                   4,5,6,
-#                   7,8,0]
-
-start_time = time.time()
 
 
 def dfs(initial_state, goal_state):
@@ -35,8 +27,7 @@ def dfs(initial_state, goal_state):
         print("Not solvable")
     first_state = [initial_state, ["start"]]
     stack = [first_state]
-    visited = []
-    
+    visited = {}
     # Loop until the stack is empty, if found return the path and amount of moves.
     while stack:
         # Pop the top state from the stack
@@ -48,20 +39,22 @@ def dfs(initial_state, goal_state):
             return (len(final_path)-1), final_path # The -1 is to remove the "start" from the path
         
         # Add the state to the explored set
-        visited.append(state[0])
+        stateAsString = ''.join(str(i) for i in state[0])
+        visited[stateAsString] = stateAsString
         # Generate the successors of the state
         neighbors = find_neighbors(state, visited)
         # Loop through the successors in reverse order 
         for successor in neighbors[::-1]:
-            # Check if the successor has not been explored 
-            if successor[0] not in visited:
+            # Check if the successor has not been explored
+            checkThis = ''.join(str(i) for i in successor[0]) 
+            if checkThis not in visited:
                 # Add the successor to the stack with the path to reach it
                 stack.append(successor)
+            
+
     # If the goal state is not found, return None
     print("Not found end state.")
     return None
-
-
 
 
 def find_neighbors(state, visited):
@@ -79,10 +72,10 @@ def find_neighbors(state, visited):
         new_state = state[0][:]
         new_state[indexLocation], new_state[indexLocation - 3] = new_state[indexLocation - 3], new_state[indexLocation]
         # Add the new state to the list of neighbors
-        if new_state not in visited:
+        new_state_check = ''.join(str(i) for i in new_state)
+        if new_state_check not in visited:
             new_path = state[1] + ["up"]
             if new_state == goal_state:
-                print("Found goal state")
                 return [(new_state, new_path)] 
             neighbors.append((new_state, new_path))
     # Check if the zero tile is not in the last row
@@ -91,9 +84,9 @@ def find_neighbors(state, visited):
         new_state = state[0][:]
         new_state[indexLocation], new_state[indexLocation + 3] = new_state[indexLocation + 3], new_state[indexLocation]
         # Add the new state to the list of neighbors
-        if new_state not in visited:
+        new_state_check = ''.join(str(i) for i in new_state)
+        if new_state_check not in visited:
             if new_state == goal_state:
-                print("Found goal state")
                 return [(new_state, new_path)]
             new_path = state[1] + ["down"]
             neighbors.append((new_state, new_path))
@@ -103,9 +96,9 @@ def find_neighbors(state, visited):
         new_state = state[0][:]
         new_state[indexLocation], new_state[indexLocation - 1] = new_state[indexLocation - 1], new_state[indexLocation]
         # Add the new state to the list of neighbors
-        if new_state not in visited:
+        new_state_check = ''.join(str(i) for i in new_state)
+        if new_state_check not in visited:
             if new_state == goal_state:
-                print("Found goal state")
                 return [(new_state, new_path)]
             new_path = state[1] + ["left"]
             neighbors.append((new_state, new_path))
@@ -115,22 +108,19 @@ def find_neighbors(state, visited):
         new_state = state[0][:]
         new_state[indexLocation], new_state[indexLocation + 1] = new_state[indexLocation + 1], new_state[indexLocation]
         # Add the new state to the list of successors
-        if new_state not in visited:
+        new_state_check = ''.join(str(i) for i in new_state)
+        if new_state_check not in visited:
             if new_state == goal_state:
-                print("Found goal state")
                 return [(new_state, new_path)]
             new_path = state[1] + ["right"]
             neighbors.append((new_state, new_path))
     return neighbors
 
 
+start_time = time.time()
 
-
-# Check if the puzzle is solvable
 try:
     cost, path = dfs(initial_puzzle, goal_state)
-    print("The path is ", path, " and the cost is ", cost, " and the time is to execute was", round((time.time() - start_time), 4), "seconds")
+    print("The path is", path, "\nThe amount of moves is " + str(cost) + ".\nThe time is to execute was", round((time.time() - start_time), 6), "seconds.")
 except:
     print("Not solvable")
-
-# More comments.
